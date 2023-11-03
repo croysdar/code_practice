ea1f64c53ec8af27a24078f11823caae88dd7319
Rebecca <rebecca@croysdale.net>
2023-11-03 15:56:51 -0700

    Adds different ways for search function to run

--- Angular/first-app/src/app/home/home.component.ts
+++ Angular/first-app/src/app/home/home.component.ts
@@ -3,20 +3,22 @@ import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
 import { HousingService } from '../housing.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    HousingLocationComponent
+    HousingLocationComponent,
+    FormsModule
   ],
   template: `
     <section>
-      <form>
-        <input type="text" placeholder="Filter by city" #filter>
+      <form (submit)="preventFormSubmission($event)">
+        <input type="text" placeholder="Filter by city" name="filter" [(ngModel)]="filterValue" (input)="filterResults(filterValue)">
         <button class="primary" type="button"
-        (click)="filterResults(filter.value)">Search</button>
+        (click)="filterResults(filterValue)">Search</button>
       </form>
     </section>
     <section class="results">
@@ -35,6 +37,7 @@ export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   filteredLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filterValue: string = '';
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
@@ -50,5 +53,10 @@ export class HomeComponent {
       housingLocation =>
       housingLocation?.city.toLowerCase().includes(text.toLowerCase())
     );
+  }
+
+  preventFormSubmission(event: Event) {
+    event.preventDefault();
+    this.filterResults(this.filterValue);
   }
 }
